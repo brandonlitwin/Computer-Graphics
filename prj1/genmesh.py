68a4bdf7a0a0cd70f65902dfd34af3e64b06af46
Brandon Litwin <brandon.litwin1@marist.edu>
2019-02-04 16:12:56 -0500

    Added more arguments

--- prj1/genmesh.py
+++ prj1/genmesh.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-g", help="generate a sphere or cylinder", action="store_true")
-parser.add_argument("geometry", help="sphere | cylinder")
+parser = argparse.ArgumentParser(prog="genmesh")
+parser.add_argument("-g", help="generate a sphere or cylinder", metavar="<sphere|cylinder>")
+parser.add_argument("-n", help="divisionsU", type=int, default=32, nargs="?", metavar="divisionsU")
+parser.add_argument("-m", help="divisionsV", type=int, default=16, nargs="?", metavar="divisionsV")
+parser.add_argument("-i", help="input a file", action="store_true")
 #parser.add_argument("-o", help="output to a file")
 args = parser.parse_args()
 if args.g:
     print("Selected geometry")
-    if args.geometry == "sphere":
+    if args.g == "sphere":
         print("Generating sphere")
-    elif args.geometry == "cylinder":
+    elif args.g == "cylinder":
         print("Generating cylinder")
     else:
         print("Invalid gemoetry (sphere | cylinder)")
+    if args.n and args.m:
+      print("Selected n and m")
+      print(args.n)
+      print(args.m)
+    elif args.n:
+        print("Selected n:")
+        print(args.n)
+    elif args.m:
+        print("Selected m:")
+        print(args.m)
+    else:
+        print("n and m not selected")
+elif args.i:
+    print("Selected input")
 else:
-    print("G not found")
+    print("Missing arguments -g or -i")
