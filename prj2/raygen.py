1c2f884cc6231fc3f232be14505203b66306500b
Brandon Litwin <brandon.litwin1@marist.edu>
2019-02-27 17:27:08 -0500

    Creating ray objects and hit records

--- prj2/raygen.py
+++ prj2/raygen.py
@@ -2,15 +2,43 @@
 # Brandon Litwin, Project 2, Computer Graphics
 import numpy as np
 import png
+import time
 
 c = np.array([0.0,0.0,2.0])
 radius = 1.0
 # Create a sphere as an array with coordinates x, y, z as 0,0,2 and radius 1.0
-sp = np.array([0.0,0.0,2.0,2.0])
+sp = np.array([0.0,0.0,2.0,1.0])
 e = np.array([0.0, 0.0, 0.0])
 resolution = 720*1280
 width = 1280
 height = 720
+
+class HitRecord:
+    def __init__(self, t, n):
+        self.time = t
+        self.normal = n
+        #self.surface = s
+
+class Light:
+    def __init__(self, p, c):
+        self.position = p
+        self.color = c
+
+"""class Scene:
+
+class Group:
+    
+class Surface:"""
+
+class Material:
+    def __init__(self, c, s):
+        self.color = c
+        self.shininess = s
+
+class Ray:
+    def __init__(self, o, d):
+        self.origin = o
+        self.distance = d
 
 def compute_ray(i,j):
     intersects_sphere = True
@@ -20,14 +48,14 @@ def compute_ray(i,j):
     b = -1
     u = l + (r-l) * ((i+0.5)/width)
     v = b + (t-b) * ((j+0.5)/height)
-    #n = 2*(p-c)
-    w = 1 # near point between the eye and sphere
-    s = e + u + v + w
+    w = -1 # point behind the eye
+    s = e + u + v + w # s = e + uU + vV + wW?
     d = s - e
     return d
 
-def find_intersection(d):
+def find_intersection(ray):
     # Computing ray-sphere intersection
+    d = ray.distance
     discriminant = np.square(np.dot(d, e-c)) - (np.dot(d,d) * (np.dot(e-c, e-c)) - (radius * radius))
     # A negative discriminant means no intersections
     if discriminant >= 0:
@@ -39,25 +67,19 @@ def find_intersection(d):
         if (p[0] <= p2[0]):
             # Compute the normal vector
             n = 2 * (p-c)
-            return (p,n)
         else:
             n = 2 * (p2-c)
-            return (p2,n)
-    else:
-        return False
+        return n
     
-
-count = 0
 for i in range(1,height+1):
     for j in range(1, width+1):
-        ray = compute_ray(i,j)
-        ray_intersection = find_intersection(ray)
-        if (ray_intersection):
-            count = count + 1
+        # Create a ray object with origin e and distance that is computed from the compute_ray function
+        ray = Ray(e,compute_ray(i,j))
+        ray_intersection_normal = find_intersection(ray)
+        if (ray_intersection_normal is not None):
+            ray.normal = ray_intersection_normal
+            localtime = time.localtime(time.time())
+            hit_record = HitRecord(localtime, ray.normal)
         
-print(count)
-# for each pixel in 720x1280, compute viewing ray
-# p(t) = e + t(s-e)
-# e is the eye location, s is the point on the screen
 
 
