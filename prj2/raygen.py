e3f237ff067117cb6de662abf7ef733929f132e4
Brandon Litwin <brandon.litwin1@marist.edu>
2019-02-28 15:18:49 -0500

    Added more class definitions and made the program work more object oriented

--- prj2/raygen.py
+++ prj2/raygen.py
@@ -2,33 +2,43 @@
 # Brandon Litwin, Project 2, Computer Graphics
 import numpy as np
 import png
-import time
 
-c = np.array([0.0,0.0,2.0])
-radius = 1.0
-# Create a sphere as an array with coordinates x, y, z as 0,0,2 and radius 1.0
-sp = np.array([0.0,0.0,2.0,1.0])
 e = np.array([0.0, 0.0, 0.0])
 resolution = 720*1280
 width = 1280
 height = 720
 
+# The class definitions
 class HitRecord:
-    def __init__(self, t, n):
+    def __init__(self, t, n, s):
         self.time = t
         self.normal = n
-        #self.surface = s
+        self.surface = s
 
 class Light:
     def __init__(self, p, c):
         self.position = p
         self.color = c
 
-"""class Scene:
+class Scene:
+    def __init__(self, g, l, c):
+        self.object_list = g
+        self.light_source = l
+        self.background_color = c
 
 class Group:
+    def __init__(self, s):
+        self.surface = s
+
+class Sphere:
+    def __init__(self, s, c, r):
+        self.surface = s
+        self.center = c
+        self.radius = r
     
-class Surface:"""
+class Surface:
+    def __init__(self, m):
+        self.material = m
 
 class Material:
     def __init__(self, c, s):
@@ -48,15 +58,18 @@ def compute_ray(i,j):
     b = -1
     u = l + (r-l) * ((i+0.5)/width)
     v = b + (t-b) * ((j+0.5)/height)
-    w = -1 # point behind the eye
-    s = e + u + v + w # s = e + uU + vV + wW?
+    w = 1 # point behind the eye
+    s = e + u + v + w 
     d = s - e
     return d
 
 def find_intersection(ray):
     # Computing ray-sphere intersection
     d = ray.distance
-    discriminant = np.square(np.dot(d, e-c)) - (np.dot(d,d) * (np.dot(e-c, e-c)) - (radius * radius))
+    c = sphere.center
+    r = sphere.radius
+
+    discriminant = np.square(np.dot(d, e-c)) - (np.dot(d,d) * (np.dot(e-c, e-c)) - (r * r))
     # A negative discriminant means no intersections
     if discriminant >= 0:
         t = (np.dot(-d, e-c) + np.sqrt(discriminant)) / np.dot(d,d)
@@ -69,17 +82,24 @@ def find_intersection(ray):
             n = 2 * (p-c)
         else:
             n = 2 * (p2-c)
-        return n
-    
+            t = t2
+        return t, n
+
+# Instantiate all classes needed for scene
+light_source = Light(np.array([1.0,5.0,1.0]), np.array([255,255,255]))
+sphere_material = Material(np.array([255, 0, 0]), 1)
+sphere_surface = Surface(sphere_material)
+sphere = Sphere(sphere_surface, np.array([0.0,0.0,-2.0]), 1.0)
+scene = Scene(sphere, light_source, np.array([0,0,0])) 
 for i in range(1,height+1):
     for j in range(1, width+1):
         # Create a ray object with origin e and distance that is computed from the compute_ray function
         ray = Ray(e,compute_ray(i,j))
-        ray_intersection_normal = find_intersection(ray)
-        if (ray_intersection_normal is not None):
-            ray.normal = ray_intersection_normal
-            localtime = time.localtime(time.time())
-            hit_record = HitRecord(localtime, ray.normal)
+        ray_intersection = find_intersection(ray)
+        if (ray_intersection is not None):
+            ray.normal = ray_intersection[0]
+            ray.time = ray_intersection[1]
+            hit_record = HitRecord(ray.time, ray.normal, sphere_surface)
         
 
 
