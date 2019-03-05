db1948c44d5fdf8b419c59522aa7ff09fbc7c556
Brandon Litwin <brandon.litwin1@marist.edu>
2019-03-05 18:17:14 -0500

    2 different color circles overlapping

--- prj2/raygen.py
+++ prj2/raygen.py
@@ -62,7 +62,6 @@ def compute_ray(i,j):
     U = 1
     V = 1
     W = 1
-    # The 2's come from the distances x and y (l,r) and (t,b)
     #s = e + u*U + v*V + w*W 
     #d = s - e
     #d = (0,0,-1)
@@ -81,30 +80,61 @@ def find_intersection(ray):
     d = ray.distance
     c = sphere.center
     r = sphere.radius
+    c2 = sphere2.center
+    r2 = sphere2.radius
 
     discriminant = np.square(np.dot(d, e-c)) - (np.dot(d,d) * (np.dot(e-c, e-c)) - (r * r))
+    discriminant2 = np.square(np.dot(d, e-c2)) - (np.dot(d,d) * (np.dot(e-c2, e-c2)) - (r2 * r2))
     #print("discr")
-    #print(discriminant)
     # A negative discriminant means no intersections
-    if discriminant >= 0:
+    if discriminant >= 0 or discriminant2 >= 0:
         #print(np.multiply(-1,d))
-        t = (np.dot(np.multiply(-1,d), e-c) + np.sqrt(discriminant)) / np.dot(d,d)
-        t2 = (np.dot(d, e-c) - np.sqrt(discriminant)) / np.dot(d,d)
-        p = e + (np.dot(t,d))
-        p2 = e + (np.dot(t2,d))
-        #print(p,p2)
-        # p is the point of intersection, we only care about the one that is closer to the eye
-        if (p[0] <= p2[0]):
-        # Compute the normal vector
-             n = 2 * (p-c)
-        else:
-            n = 2 * (p2-c)
-            t = t2
-        return t, n
+        t = None
+        t_2 = None
+        p = None
+        p2 = None
+        if discriminant >= 0:
+            t = (np.dot(np.multiply(-1,d), e-c) + np.sqrt(discriminant)) / np.dot(d,d)
+            t2 = (np.dot(np.multiply(-1,d), e-c) - np.sqrt(discriminant)) / np.dot(d,d)
+        if discriminant2 >= 0:
+            t_2 = (np.dot(np.multiply(-1,d), e-c2) + np.sqrt(discriminant2)) / np.dot(d,d)
+            t2_2 = (np.dot(np.multiply(-1,d), e-c2) - np.sqrt(discriminant2)) / np.dot(d,d)
+        if t is not None:
+            if t < t2:
+                p = e + (np.dot(t,d))
+            else:
+                p = e + (np.dot(t2,d))
 
-def evaluate_shading(n):
+        if t_2 is not None:
+            if t_2 < t2_2:
+                p2 = e + (np.dot(t_2,d))
+            else:
+                p2 = e + (np.dot(t2_2,d))
+
+        # p is the point of intersection, we only care about the one that is closer to the eye
+        if p is not None:
+            if p2 is not None:
+                if p[0] <= p2[0]:
+                # Compute the normal vector
+                    n = 2 * (p-c)
+                else:
+                    n = 2 * (p2-c2)
+                    t = t2
+            else:
+                n = 2 * (p-c)
+                if t2 < t:
+                    t = t2
+            return t, n, sphere
+        elif p2 is not None:
+            n = 2 * (p2-c2)
+            if t2_2 < t_2:
+                t_2 = t2_2
+            return t_2, n, sphere2
+
+
+def evaluate_shading(n, intersecting_sphere):
     i = light_source.color
-    k = sphere.surface.material.color 
+    k = intersecting_sphere.surface.material.color 
     # l is the vector pointing from p to the light
     p = (n/2) + sphere.center
     l = p - light_source.position
@@ -115,14 +145,20 @@ def evaluate_shading(n):
     print(shaded_color)"""
     pixel_color = k #*i*max(0, np.dot(n,l))
     #KdId = (rk x rI, gK x gI, bk x bI)
+    #print(pixel_color)
     return pixel_color
 
 # Instantiate all classes needed for scene
 light_source = Light(np.array([1.0,5.0,1.0]), np.array([255,255,255]))
 sphere_material = Material([255, 0, 0], 1)
+sphere2_material = Material([0, 0, 255], 1)
 sphere_surface = Surface(sphere_material)
+sphere2_surface = Surface(sphere2_material)
 sphere = Sphere(sphere_surface, np.array([0.0,0.0,-5.0]), 1.0)
-scene = Scene(sphere, light_source, [0,0,0]) 
+sphere2 = Sphere(sphere2_surface, np.array([-1.0,0.0,-5.0]), 1.0)
+group = Group([sphere.surface, sphere2.surface])
+scene = Scene(group, light_source, [0,0,0])
+#print(scene.object_list.surface[1].material.color)
 # Create array for pixel colors
 #pixel_colors = np.empty([resolution])
 #np.append(pixel_colors, np.array([0,0,0]))
@@ -137,20 +173,21 @@ for i in range(1,height+1):
         if (ray_intersection is not None):
             intersection_time = ray_intersection[0]
             intersection_normal = ray_intersection[1]
-            hit_record = HitRecord(intersection_time, intersection_normal, sphere_surface)
+            intersecting_sphere = ray_intersection[2]
+            hit_record = HitRecord(intersection_time, intersection_normal, intersecting_sphere.surface)
             #pixel_colors = np.append(pixel_colors, evaluate_shading(intersection_normal)) 
             #np.insert(pixel_colors, count, evaluate_shading(intersection_normal))
             #pixel.append(evaluate_shading(intersection_normal)[0], evaluate_shading(intersection_normal)[1], evaluate_shading(intersection_normal)[2])
-            pixel_colors.append(evaluate_shading(intersection_normal)[0])
-            pixel_colors.append(evaluate_shading(intersection_normal)[1])
-            pixel_colors.append(evaluate_shading(intersection_normal)[2])
+            pixel_colors.append(evaluate_shading(intersection_normal, intersecting_sphere)[0])
+            pixel_colors.append(evaluate_shading(intersection_normal, intersecting_sphere)[1])
+            pixel_colors.append(evaluate_shading(intersection_normal, intersecting_sphere)[2])
 
         else:
             #pixel_colors = np.append(pixel_colors, scene.background_color)
             #pixel.append(scene.background_color[0],scene.background_color[1],scene.background_color[2])
             pixel_colors.append(scene.background_color[0])
-            pixel_colors.append(scene.background_color[1])
-            pixel_colors.append(scene.background_color[2])
+            pixel_colors.append(scene.background_color[1]) 
+            pixel_colors.append(scene.background_color[2]) 
         #pixel_row.append(pixel)
     #print(pixel_row)        
     #pixel_colors.append(pixel_row)
