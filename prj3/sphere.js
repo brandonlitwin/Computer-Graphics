546097204909ca961ad091770b9646874e09624f
Brandon Litwin <brandon.litwin1@marist.edu>
2019-04-03 15:18:54 -0400

    Updated position and normal. Started on custom

--- prj3/sphere.js
+++ prj3/sphere.js
@@ -92,7 +92,7 @@ window.onload = function init() {
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    gl.clearColor( 255.0, 255.0, 255.0, 1.0 );
 
     gl.enable(gl.DEPTH_TEST);
 
